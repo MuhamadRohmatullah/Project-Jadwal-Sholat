8b09153c4b9ed669eeed7da8cdb202e23628d327
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-29 03:18:51 +0700

    Membuat component select wilayah unutk menentukah jadwal wilayah, menambahkan select option dalam span dan menambahkan v-model unutk menampung value dalam data wilayah.

---
+++ resources/js/components/SelectWilayahComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <select
+      class="form-select form-select-sm fs-5 shadow-sm"
+      aria-label="Small select example"
+      v-model="wilayah"
+    >
+      <option value="jakartapusat" selected>Jakarta</option>
+      <option value="sukabumi">Sukabumi</option>
+      <option value="bandung">Bandung</option>
+      <option value="surabaya">Surabaya</option>
+      <option value="denpasar">Denpasar</option>
+      <option value="medan">Medan</option>
+    </select>
+  </span>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      wilayah: "jakartapusat",
+      id: "",
+    };
+  },
+};
+</script>
