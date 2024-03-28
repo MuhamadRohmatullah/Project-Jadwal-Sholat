ce64dabc0dfe9c382a52d79c6d9f8530d7cf6a6f
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-29 03:28:39 +0700

    Membuat tombol unutk menampillkan ayat - ayat alquran yang berneda dan mentriger method ganti yang memiliki parameter no unutk memnentukan surat yang akan ditampilkan.

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -3,7 +3,7 @@
     <div class="card-body text-center">
       <div class="row">
         <div class="col-1 me-0 ms-0 pt-6">
-        
+          <v-icon icon="mdi-chevron-left" size="30px" @click="ganti(2)" />
         </div>
         <div class="col">
           <blockquote class="blockquote">
@@ -15,7 +15,7 @@
           </blockquote>
         </div>
         <div class="col-1 me-0 ms-0 pt-6">
-         
+          <v-icon icon="mdi-chevron-right" size="30px" @click="ganti(1)" />
         </div>
       </div>
       <span>
@@ -27,7 +27,11 @@
 <script>
 import { mapGetters } from "vuex";
 export default {
- 
+  methods: {
+    ganti(no) {
+      this.$store.dispatch("getSurah", no);
+    },
+  },
   computed: {
     ...mapGetters({
       surah: "getCaption",
