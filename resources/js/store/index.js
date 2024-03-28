db71632c0c9159c15184cb37c59ca8f1467902d8
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-29 04:14:21 +0700

    mengimport axios, membuat sctions getjadwal unutk mengambil data jadwal sholat dari api dengan axios, menambahkan beberapa parameter yang dinamis sesuai masukan dari dispacth select wilayah, dan parameter unutk menampilkan bulan sebelumnya atau bulan sekarang. memasukan beberapa data dari response ke jadwal perhari unutk mengambil data di tanggal hari ini, dan memasukan data utuh ke jadwal. menambhakan getters get data unutk mengambil state jadwal di component.

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,16 +1,69 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
     state: {
-        
+        jadwal: {}
     },
     getters: {
-       
+        getData(state) {
+            return state.jadwal;
+        }
     },
     actions: {
-       
+        getJadwal: async (context, payload) => {
+            let bulan = new Date().getMonth();
+            let cnt = 0;
+            let wilayah = 'jakartapusat';
+
+            if (payload == 'bandung') {
+                wilayah = 'bandung';
+            } else if (payload == 'surabaya') {
+                wilayah = 'surabaya';
+            } else if (payload == 'sukabumi') {
+                wilayah = 'sukabumi';
+            } else if (payload == 'denpasar') {
+                wilayah = 'denpasar';
+            } else if (payload == 'medan') {
+                wilayah = 'medan';
+            } else {
+                wilayah = 'jakartapusat'
+            }
+
+
+
+            if (payload == 1) {
+                cnt += 1;
+                bulan - cnt;
+            } else if (payload == 2) {
+                bulan += 1;
+            } else {
+                bulan += 1;
+            }
+            if (bulan < 10) {
+                bulan = '0' + bulan;
+
+            }
+            let tanggal = new Date().getFullYear() + '/' + bulan;
+            let response = await axios.get('https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/' + wilayah + '/' + tanggal + '.json');
+
+            context.commit('ADD_DATA', response.data);
+        }
     },
     mutations: {
-       
+        ADD_DATA(state, payload) {
+            let list = {
+                imsyak: payload[new Date().getDate() - 1].imsyak,
+                shubuh: payload[new Date().getDate() - 1].shubuh,
+                terbit: payload[new Date().getDate() - 1].terbit,
+                dhuha: payload[new Date().getDate() - 1].dhuha,
+                dzuhur: payload[new Date().getDate() - 1].dzuhur,
+                ashr: payload[new Date().getDate() - 1].ashr,
+                magrib: payload[new Date().getDate() - 1].magrib,
+                isya: payload[new Date().getDate() - 1].isya
+            }
+            state.jadwalPerhari = list,
+                state.jadwal = payload;
+        }
     }
 })
