b4505bbbf29d50f92904fb72fd8eae8c897b498a
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-29 03:08:20 +0700

    Membuat header component, menambahkan navbar, container fluid dan menambahkan icon dari mdi vuetify. memberi style css.

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar bg-body-tertiary shadow-sm sticky-top" id="navigasi">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bold fs-3" id="brand"
+        ><v-icon icon="$vuetify" /> Netepan
+      </a>
+     
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+ 
+};
+</script>
+<style>
+#brand {
+  font-family: "Sriracha", cursive;
+}
+
+#navigasi {
+  background-color: #24da76 !important;
+}
+
+span,
+#brand {
+  cursor: pointer;
+}
+
+</style>
