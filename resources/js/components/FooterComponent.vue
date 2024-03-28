d11859b97dc8a7dc8d9474f63082de89ddafcbba
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-29 03:24:15 +0700

    Membuat footer component unutk bagian footer halaman main, menambahkan card dan grid unutk menampilkan ayat al-quran.

---
+++ resources/js/components/FooterComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card mt-3 shadow-sm" id="footer">
+    <div class="card-body text-center">
+      <div class="row">
+        <div class="col-1 me-0 ms-0 pt-6">
+         
+        </div>
+        <div class="col">
+          <blockquote class="blockquote">
+            <p></p>
+            <br />
+            <p></p>
+            <br />
+            <p></p>
+          </blockquote>
+        </div>
+        <div class="col-1 me-0 ms-0 pt-6">
+         
+        </div>
+      </div>
+      <span>
+      
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+ 
+};
+</script>
+<style>
+#footer {
+  background-color: #24da76;
+  color: white;
+}
+</style>
